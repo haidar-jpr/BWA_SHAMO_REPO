6ab7b93b8338f83fa44a71886dfbaa6a2ebbab59
Subhan Ikraam Haidar <haidar.dod@gmail.com>
2021-12-26 06:39:29 +0700

    menambahkan bagian content pada page detail products

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -11,6 +11,18 @@ class ProductsPage extends StatelessWidget {
     'assets/img_shoes.png',
   ];
 
+  final List familiarShoes = [
+    'assets/img_shoes.png',
+    'assets/img_shoes.png',
+    'assets/img_shoes.png',
+    'assets/img_shoes.png',
+    'assets/img_shoes.png',
+    'assets/img_shoes.png',
+    'assets/img_shoes.png',
+    'assets/img_shoes.png',
+    'assets/img_shoes.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +32,223 @@ class ProductsPage extends StatelessWidget {
           ImageSliderProducts(
             images: images,
           ),
+          ContentProduct(familiarShoes: familiarShoes),
         ],
+      ),
+    );
+  }
+}
+
+class ContentProduct extends StatelessWidget {
+  const ContentProduct({
+    Key? key,
+    required this.familiarShoes,
+  }) : super(key: key);
+
+  final List familiarShoes;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(
+        top: 17,
+      ),
+      padding: EdgeInsets.all(
+        defaultMargin,
+      ),
+      decoration: BoxDecoration(
+        color: bgColor1,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(24),
+          topRight: Radius.circular(24),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'TERREX URBAN LOW',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  Text(
+                    'Hiking',
+                    style: subtitleTextStyle.copyWith(
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Image.asset(
+                  'assets/icon_wistlist.png',
+                  width: 46,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: defaultMargin - 10,
+            ),
+            padding: EdgeInsets.all(
+              16,
+            ),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: bgColor2,
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Price starts from',
+                  style: primaryTextStyle,
+                ),
+                Text(
+                  '\$143,98',
+                  style: priceTextStyle.copyWith(
+                    fontWeight: semiBold,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: defaultMargin,
+          ),
+          Text(
+            'Description',
+            style: primaryTextStyle.copyWith(fontWeight: medium),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Container(
+            child: Text(
+              "Unpaved trails and mixed surfaces are easy when you have the traction and support you need. Casual enough for the daily commute.",
+              style: subtitleTextStyle.copyWith(
+                fontWeight: light,
+              ),
+              textAlign: TextAlign.justify,
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(
+              top: defaultMargin,
+              bottom: 12,
+            ),
+            child: Text(
+              'Fimiliar Shoes',
+              style: primaryTextStyle.copyWith(
+                fontWeight: medium,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: familiarShoes
+                  .map(
+                    (image) => FamiliarCardProducts(
+                      imageUrl: image,
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(
+              top: defaultMargin,
+            ),
+            child: Row(
+              children: [
+                OutlinedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/detail-chat');
+                  },
+                  style: OutlinedButton.styleFrom(
+                    padding: EdgeInsets.all(16),
+                    side: BorderSide(
+                      width: 1,
+                      color: primaryColor,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Image.asset(
+                    'assets/icon_open_chat.png',
+                    width: 23,
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 12,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      primary: primaryColor,
+                    ),
+                    child: Text(
+                      'Add to Cart',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FamiliarCardProducts extends StatelessWidget {
+  const FamiliarCardProducts({Key? key, required this.imageUrl})
+      : super(key: key);
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 54,
+      margin: EdgeInsets.only(
+        right: 16,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(6),
+        child: Image.asset(
+          imageUrl,
+        ),
       ),
     );
   }
