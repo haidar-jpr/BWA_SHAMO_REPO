07d2394ab094e067156f6a2b9e959f38688f2133
Haidar-JPR <haidar.dod@gmail.com>
2022-03-03 14:02:04 +0700

    add transaction service for checkout page

---
+++ lib/services/transaction_service.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:shamo/models/cart_model.dart';
+
+class TransactionService {
+  String baseURL = 'https://shamo-backend.buildwithangga.id/api';
+
+  Future<bool> checkout(
+      String token, List<CartModel> carts, double totalPrice) async {
+    var url = Uri.parse('$baseURL/checkout');
+    var headers = {
+      'Content-Type': 'application-json',
+      'Authorization': token,
+    };
+
+    var body = jsonEncode(
+      {
+        'address': 'Marsemoon',
+        'items': carts
+            .map(
+              (cart) => {
+                'id': cart.products!.id,
+                'quantity': cart.quantity,
+              },
+            )
+            .toList(),
+        'status': 'PENDING',
+        'total_price': totalPrice,
+        'shipping_price': 0,
+      },
+    );
+
+    var response = await http.post(url,headers: headers,body: body);
+
+    print(response.body);
+
+    if(response.statusCode == 200){
+      return true;
+    }else{
+      throw Exception('Gagal checkout!');
+    }
+  }
+}
