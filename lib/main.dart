3a8b6c8e245db0aa779aeb74e3e3632889b68a3c
Haidar-JPR <haidar.dod@gmail.com>
2022-03-05 05:09:49 +0700

    add google firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shamo/pages/cart_page.dart';
@@ -16,8 +18,10 @@ import 'package:shamo/providers/products_provider.dart';
 import 'package:shamo/providers/transaction_provider.dart';
 import 'package:shamo/providers/wishlist_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
